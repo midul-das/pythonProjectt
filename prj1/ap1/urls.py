084d70ea7f8604cdb79062f05b97a55c7c042546
Admin <mriduldascprasoonam@gmail.com>
2022-03-14 16:26:53 +0530

    first commit

---
+++ prj1/ap1/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path,include
+from .views import aa
+
+urlpatterns = [
+
+    path('aa/', aa),
+
+]
---
+++ prj1/ap1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def aa(request):
+    return render(request,"a1.html")
